7c56ad5fb76ad8bad750adce0003538e1f93e920
pauloffrr <pauloffr1@gmail.com>
2024-08-16 21:51:41 -0300

    convertendo segundos em horas, e horas em segundos

---
+++ SegundosEmHoras.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SegundosEmHoras {
+    public static void main(String[] args) {
+        Scanner prompt = new Scanner(System.in);
+        System.out.print("Informe os segundos: ");
+        int segundos = prompt.nextInt();
+        int horas = segundos/60/60;
+        segundos= segundos - (horas*60*60);
+        int minutos= segundos/60;
+        segundos= segundos - (minutos*60);
+        System.out.println("Horas: "+horas+" Minutos: "+minutos+" Segundos: "+segundos);
+
+    }
+}
--- index.java
+++ index.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class index {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("não");
+        Scanner prompt = new Scanner(System.in);
+        System.out.print("Informe as horas: ");
+        int horas = prompt.nextInt();
+        System.out.print("Informe os minutos: ");
+        int minutos = prompt.nextInt();
+        System.out.print("Informe os segundos: ");
+        int segundos = prompt.nextInt();
+        int resultado= (horas*60*60) + (minutos*60) + segundos;
+        System.out.println("Segundos: "+resultado);
     }
+
 }
